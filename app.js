4c0aa80320daa195f2dd6a44c08f5f1e0fb8ea94
Joseph Mendoza <josephmendoza23@gmail.com>
2018-07-09 18:10:49 -0400

    created basic note taking functions and implemented yargs

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 console.log("Starting app.js");
 
+const fs = require("fs");
+const _ = require("lodash");
+const yargs = require("yargs");
+
 const notes = require("./notes");
 
-notes.add();
-notes.remove();
+const argv = yargs.argv;
+const command = argv._[0];
+console.log("Command: ", command);
+console.log("Yargs: ", argv);
+
+if (command === "add") {
+  notes.addNote(argv.title, argv.body);
+} else if (command === "list") {
+  notes.getAll();
+} else if (command === "read") {
+  notes.getNote(argv.title);
+} else if (command === "remove") {
+  notes.removeNotes(argv.title);
+} else {
+  console.log("Command not recognized");
+}
--- notes.js
+++ notes.js
@@ -1,10 +1,23 @@
-console.log("Starting notes.js");
+const addNote = (title, body) => {
+  console.log(`Title: ${title}
+Body: ${body}`);
+};
+
+const getAll = () => {
+  console.log(`Getting all notes!`);
+};
+
+const getNote = title => {
+  console.log(`Getting ${title}`);
+};
+
+const removeNote = title => {
+  console.log(`Removing ${title}`);
+};
 
 module.exports = {
-  add() {
-    console.log("adding new note");
-  },
-  remove() {
-    console.log("removing note");
-  }
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
