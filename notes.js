dfebf574d2aa435ddec98e43ad08a75f78260f38
Joseph Mendoza <josephmendoza23@gmail.com>
2018-07-11 16:59:15 -0400

    implemented addNote functionality to store new notes in a json text file

--- notes.js
+++ notes.js
@@ -1,6 +1,28 @@
+console.log("Starting notes.js");
+
+const fs = require("fs");
+
 const addNote = (title, body) => {
-  console.log(`Title: ${title}
-Body: ${body}`);
+  let notes = [];
+  const note = {
+    title,
+    body
+  };
+
+  try {
+    const notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+  } catch (error) {}
+
+  const duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+    console.log("Note added");
+  } else {
+    console.log(`Error: a note with the title ${note.title} already exists`);
+  }
 };
 
 const getAll = () => {
